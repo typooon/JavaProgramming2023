7a2cb50010981078563263c2ea6e1caa053b6498
typooon <scotte0123@g.shingu.ac.kr>
2023-09-20 13:01:14 +0900

    v7

--- week03/src/Pokemon.java
+++ week03/src/Pokemon.java
@@ -71,5 +71,12 @@ public class Pokemon {
         pokemon.level = pokemon.level + 1;
         System.out.println(pokemon.name + "의 레벨이 " + pokemon.level + "으로 증가!");
     }
+    void attack(){
+        System.out.println(this.name + "이(가) 광역 공격을 시전합니다.");
+    }
+    void attack(Pokemon pokemon){
+        System.out.println(this.name + "이(가)"+ pokemon.name +"에게 기본 공격을 시전합니다.");
+    }
+
 
 }
