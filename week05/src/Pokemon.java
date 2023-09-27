c238f706f2e3eee24fa60b56000c3f593a25641a
typooon <scotte0123@g.shingu.ac.kr>
2023-09-27 14:19:15 +0900

    v23

--- week05/src/Pokemon.java
+++ week05/src/Pokemon.java
@@ -53,7 +53,12 @@ public abstract class Pokemon {//abstract 는 추상클래스, 객체 생성 불
 
     public void attack(Pokemon targetPokemon, String skill){
         System.out.println(this.name + "이(가) " + targetPokemon.name + "에게" + skill +"공격!");
-        targetPokemon.hp = targetPokemon.hp- (this.attackRate - targetPokemon.defenceRate);
+        int temporaryAttackRate = this.attackRate - targetPokemon.defenceRate;
+        if (temporaryAttackRate < 0 ) {
+            temporaryAttackRate = 0;
+        }
+        targetPokemon.hp = targetPokemon.hp-temporaryAttackRate;
+
         if(targetPokemon.hp <= 0) {
             System.out.println(targetPokemon.name + "은(는) 사망!");
         }else{
