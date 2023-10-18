53d83fa15f7ada596d3f55c8202d5fb7f77b5dc0
typooon <scotte0123@g.shingu.ac.kr>
2023-10-18 14:32:54 +0900

    v31

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -68,6 +68,9 @@ public class PokemonGame {
         } catch (NullPointerException err) {
             System.out.println("플레이어 객체가 생성되지않았습니다.");
             System.out.println("예외내용 : "+ err.getMessage());
+        }catch (IndexOutOfBoundsException err) {
+            System.out.println("존재하지 않는 스킬입니다.");
+            System.out.println("예외 내용 : " + err.getMessage());
         }catch (Exception err){
             System.out.println("예외가 발생했습니다.");
             System.out.println("예외 내용 : " + err.getMessage());
